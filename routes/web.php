9bab3bcf07798e84bb56122ce03089bf3627c52d
Pedro Vidal <pedrorvidal@gmail.com>
2025-05-01 19:50:52 -0300

    configurado conexao mysql

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','home');
+// Route::view('/','home');
+
+Route::get('/', function () {
+    try {
+        DB::connection()->getPdo();
+        echo "Conexão OK " . DB::connection()->getDatabaseName();
+    } catch (\Throwable $th) {
+        throw $th;
+    }
+});
