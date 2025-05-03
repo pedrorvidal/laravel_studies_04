741d4760de204baf7b20a3d44d37a6eb09fedcb0
Pedro Vidal <pedrorvidal@gmail.com>
2025-05-02 22:37:52 -0300

    migrations from users table

--- database/migrations/2025_05_02_014115_create_users_table.php
+++ database/migrations/2025_05_02_014115_create_users_table.php
@@ -12,8 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // coluna ID
+            $table->string('username', 100)->nullable();
+            $table->string('password', 200)->nullable();
+            $table->boolean('active')->default(true);
+            $table->timestamps(); // 2 colunas created_at e updated_at
+            $table->softDeletes(); // create column deleted_at
         });
     }
 
