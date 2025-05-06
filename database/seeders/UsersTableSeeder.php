2bf8ebcfbfc5e6b295f5aadf9e3298f7adee1d66
Pedro Vidal <pedrorvidal@gmail.com>
2025-05-06 00:08:46 -0300

    seeding using for

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UsersTableSeeder extends Seeder
 {
@@ -19,20 +20,29 @@ class UsersTableSeeder extends Seeder
         //     'active' => true,
         // ]);
 
-        $users = [
-            [
-                'username' => 'user2',
-                'password' => bcrypt('password2'),
-            ],
-            [
-                'username' => 'user3',
-                'password' => bcrypt('password3'),
-            ],
-            [
-                'username' => 'user4',
-                'password' => bcrypt('password4'),
-            ],
-        ];
+        // $users = [
+        //     [
+        //         'username' => 'user2',
+        //         'password' => bcrypt('password2'),
+        //     ],
+        //     [
+        //         'username' => 'user3',
+        //         'password' => bcrypt('password3'),
+        //     ],
+        //     [
+        //         'username' => 'user4',
+        //         'password' => bcrypt('password4'),
+        //     ],
+        // ];
+        // DB::table('users')->insert($users);
+
+        // 3. add random data
+        for ($i = 0; $i < 10; $i++) {
+            $users[] = [
+                'username' => 'user' . Str::random(10),
+                'password' => bcrypt('password'),
+            ];
+        }
         DB::table('users')->insert($users);
     }
 }
