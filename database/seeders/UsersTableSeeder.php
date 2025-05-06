466b3f612bbee75687ff7b633d2c107dd0427165
Pedro Vidal <pedrorvidal@gmail.com>
2025-05-05 23:54:36 -0300

    seeders and seeding database

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // DB::table('users')->insert([
+        //     'username' => 'user1',
+        //     'password' => bcrypt('password1'),
+        //     'active' => true,
+        // ]);
+    }
+}
